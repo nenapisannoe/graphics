a4c921da916835d5b3c0c3b76ddf993d2d192fee
Yana <58274732+Ignitesss@users.noreply.github.com>
2022-09-27 21:13:05 +0300

    Added task 2 (both algorithms) from lab 3

---
+++ LAB3/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace LAB3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(4, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(909, 630);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(933, 92);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bresenham";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Monotype Corsiva", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(933, 190);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(256, 68);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Wu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1205, 638);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ LAB3/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace LAB3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Point start_pos;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bitmap;
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            start_pos = e.Location;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!button1.Enabled)
+                draw_Bresenham_line(start_pos, e.Location);
+            else draw_Wu_line(start_pos, e.Location);
+            pictureBox1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+
+        void draw_Bresenham_line(Point p1, Point p2)
+        {
+            int x1 = p1.X;
+            int x2 = p2.X;
+            int y1 = p1.Y;
+            int y2 = p2.Y;
+
+            var delta = Math.Abs(y2 - y1) > Math.Abs(x2 - x1);
+                                                             
+            if (delta)
+            {
+                (x1, y1) = (y1, x1);
+                (x2, y2) = (y2, x2);
+            }
+
+            if (x1 > x2)
+            {
+                (x1, x2) = (x2, x1);
+                (y1, y2) = (y2, y1);
+            }
+
+            int dx = x2 - x1;
+            int dy = Math.Abs(y2 - y1);
+
+            int error = dx / 2;
+            int ystep = (y1 < y2) ? 1 : -1;
+            int y = y1;
+
+            var bitmap = new Bitmap(pictureBox1.Image);
+            for (int x = x1; x <= x2; x++)
+            {
+                my_set_pixel(bitmap, Color.DarkOrange, delta, x, y, 255);
+                error -= dy;
+                if (error < 0)
+                {
+                    y += ystep;
+                    error += dx;
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        void draw_Wu_line(Point p1, Point p2)
+        {
+            int x1 = p1.X;
+            int x2 = p2.X;
+            int y1 = p1.Y;
+            int y2 = p2.Y;
+
+            var delta = Math.Abs(y2 - y1) > Math.Abs(x2 - x1);
+
+            if (delta)
+            {
+                (x1, y1) = (y1, x1);
+                (x2, y2) = (y2, x2);
+            }
+
+            if (x1 > x2)
+            {
+                (x1, x2) = (x2, x1);
+                (y1, y2) = (y2, y1);
+            }
+
+            var bitmap = new Bitmap(pictureBox1.Image);
+
+            float dx = x2 - x1;
+            float dy = y2 - y1;
+            float grad_k = dy / dx;
+            float y = y1 + grad_k;
+
+            my_set_pixel(bitmap, Color.DarkSeaGreen, delta, x1, y1, 255); 
+            my_set_pixel(bitmap, Color.DarkSeaGreen, delta, x2, y2, 255);
+
+            for (var x = x1 + 1; x <= x2 - 1; x++)
+            {
+                my_set_pixel(bitmap, Color.DarkSeaGreen, delta, x, (int)y, (int)(255 * (1 - (y - (int)y))));
+                my_set_pixel(bitmap, Color.DarkSeaGreen, delta, x, (int)(y + 1), (int)(255 * (y - (int)y)));
+                y += grad_k;
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        static void my_set_pixel(Bitmap b, Color col, bool delta, int x, int y, int color_k)
+        {
+            
+            if (delta)
+                (x, y) = (y, x);
+            b.SetPixel(x, y, Color.FromArgb(color_k, col));
+        }
+    }
+}
